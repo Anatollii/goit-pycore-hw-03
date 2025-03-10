5f2fae6bbf0b0c8724d90772f057698214574753
Anatollii <anatoliivorobiov1@gmail.com>
2025-03-10 03:12:34 +0200

    Process user input

--- hw-1.py
+++ hw-1.py
@@ -1,11 +1,16 @@
 from datetime import datetime
-try:
-    def get_days_from_today(date: str) -> str:
-        input_date = datetime.strptime(date, "%Y-%m-%d").date()
-        today_date = datetime.today().date()
-        delta = today_date - input_date
-        return delta.days
+import sys
 
-except ValueError:
-    raise ValueError("Некорректный формат даты. Используйте формат 'YYYY-MM-DD'.")
-print(get_days_from_today("2025-03-15"))
+def get_days_from_today(date: str) -> str:
+    input_date = datetime.strptime(date, "%Y-%m-%d").date()
+    today_date = datetime.today().date()
+    delta = today_date - input_date
+    return delta.days
+
+if len(sys.argv) < 2:
+    print("No argument provided. Please provide an argument.")
+else:
+    try:
+        print("Days:", get_days_from_today(sys.argv[1]))
+    except ValueError as e:
+        print("Input error:", e)
