56907b6c8ce5c06d5f689b7c9f5d61937aded280
Anatollii <anatoliivorobiov1@gmail.com>
2025-03-11 03:37:54 +0200

    1

--- hw-2.py
+++ hw-2.py
@@ -1,7 +1,6 @@
 import random
 def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
     if not (1 <= min <= max <= 1000 and min <= quantity <= (max - min + 1)):
-    #if (1 <= min <= max <= 1000) or not (min <= quantity <= (max - min + 1)):
         return []
     numbers = random.sample(range(min, max+1), quantity)
     return sorted(numbers)
